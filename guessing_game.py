56aad2f02d7431af5d84257ff73122ff893bc2c3
TreySmithIII <56052484+TreySmithIII@users.noreply.github.com>
2020-07-30 16:07:36 -0500

    ?

--- guessing_game.py
+++ guessing_game.py
@@ -13,48 +13,48 @@ import random
 
 
 def start_game():
-  #  """Psuedo-code Hints
+    #"""Psuedo-code Hints
     
-  #  When the program starts, we want to:
- #   ------------------------------------
-  #  1. Display an intro/welcome message to the player.
-    print("Hello Welcome to my number guessing game !")
-   # 2. Store a random number as the answer/solution.
-    random_number= random.randint(1,10)
-   # 3. Continuously prompt the player for a guess.
-    guess = int(input("Pick a number between 1-10  "))
+    #When the program starts, we want to:
+    #------------------------------------
+    #1. Display an intro/welcome message to the player.
+    print("Hello and welcome to the random number game")
+    #2. Store a random number as the answer/solution.
+    randoms=random.randint(1,10)
     attempt = 1
-    while guess != random_number:
+    # 3. Continuously prompt the player for a guess.
+    while True:
+        try:
+            guess =input("Please pick a number between 1-10: ")
+            my_guess=int(guess)
+
+        except:
+            print("Sorry wrong Value please pick a number between 1 and 10")
+        else:
+    #  a. If the guess greater than the solution, display to the player "It's lower"
+            if randoms < my_guess:
+                print("It's lower")
+                attempt += 1
+                continue
+        #     b. If the guess is less than the solution, display to the player "It's higher".
+            elif randoms > my_guess:
+                print("It's Higher")
+                attempt+= 1
+                continue
         
-        
-     # a. If the guess greater than the solution, display to the player "It's lower".
-        if guess == random_number:
-            print("You got it ! congrats" )
-            print("it took you {} to get it not bad".format(attempt))
-            print("until next time")
-    #  b. If the guess is less than the solution, display to the player "It's higher".
-        elif guess<random_number:
-            print("its higher")
-            guess = int(input("please try again:  "))
-            attempt += 1
+        #4. Once the guess is correct, stop looping, inform the user they "Got it"
+            elif my_guess == randoms:
+                print("Got it")
+                print("It took you {} tries to get it right".format(attempt))
+                print("Till next time bye ")
+                break
+        #    and show how many attempts it took them to get the correct number.
+    #5. Let the player know the game is ending, or something that indicates the game is over.
     
-   # 4. Once the guess is correct, stop looping, inform the user they "Got it"
-        elif guess >random_number:
-            print("its lower" )
-            guess = int(input("please try again:  "))
-            attempt += 1
-            
-    #     and show how many attempts it took them to get the correct number.
-   # 5. Let the player know the game is ending, or something that indicates the game is over.
-    
-   # ( You can add more features/enhancements if you'd like to. )
- #   """
-#    # write your code inside this function.
+    #( You can add more features/enhancements if you'd like to. )
+    #"""
+    # write your code inside this function.
 
-
-
-# Kick off the program by calling the start_game function.
-start_game()
 
 
 # Kick off the program by calling the start_game function.
